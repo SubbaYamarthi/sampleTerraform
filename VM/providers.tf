493e0605757f3d4e955c52fb535ab03f0d6204bc
SubbaYamarthi <116677540+SubbaYamarthi@users.noreply.github.com>
2023-01-28 14:44:40 +0530

    Add files via upload
    sample terraform

---
+++ VM/providers.tf
@@ -0,0 +1,17 @@
+
+# #resource group created
+# #managed configuration->RG->Resources-stgaccnt->vnet->subvnet
+# terraform {
+#   required_providers {
+
+#     azurerm = {
+
+#       source  = "hashicorp/azurerm"
+#       version = "~>2.99.0"
+#     }
+#   }
+# }
+# provider "azurerm" {
+#   features {}
+
+# }
